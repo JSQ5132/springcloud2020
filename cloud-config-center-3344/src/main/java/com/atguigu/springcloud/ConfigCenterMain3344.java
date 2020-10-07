4fd4ba3130023720c57c7323ac3a37e6f605b212
Mr.Jiang <943261318@qq.com>
2020-10-07 22:06:20 +0800

    第75节完

--- cloud-config-center-3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
+++ cloud-config-center-3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
@@ -1,9 +1,18 @@
 package com.atguigu.springcloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
 /**
  * @Author JSQ
  * @Date 2020/10/7 21:29
  * @Version 1.0
  */
+@SpringBootApplication
+@EnableConfigServer
 public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344 .class,args);
+    }
 }
