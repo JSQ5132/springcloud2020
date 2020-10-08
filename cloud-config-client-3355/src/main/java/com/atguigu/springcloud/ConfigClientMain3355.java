28354c119e019306fe6a7dec3885ea817acfbbf3
Mr.Jiang <943261318@qq.com>
2020-10-08 12:32:31 +0800

    第77节完

--- cloud-config-client-3355/src/main/java/com/atguigu/springcloud/ConfigClientMain3355.java
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/ConfigClientMain3355.java
@@ -2,17 +2,15 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * @Author JSQ
- * @Date 2020/10/7 21:29
+ * @Date 2020/10/8 11:26
  * @Version 1.0
  */
 @SpringBootApplication
-@EnableConfigServer  //激活配置中心
-public class ConfigCenterMain3344 {
+public class ConfigClientMain3355 {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigCenterMain3344 .class,args);
+        SpringApplication.run( ConfigClientMain3355.class,args);
     }
 }
---
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author JSQ
+ * @Date 2020/10/8 11:27
+ * @Version 1.0
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
