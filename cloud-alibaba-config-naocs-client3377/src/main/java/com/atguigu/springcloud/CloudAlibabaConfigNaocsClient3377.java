d0def00079c32d6b08bf7f27507c9bc472f3baa8
Mr.Jiang <943261318@qq.com>
2022-03-01 22:34:02 +0800

    增加nacos配置中心模块

---
+++ cloud-alibaba-config-naocs-client3377/src/main/java/com/atguigu/springcloud/CloudAlibabaConfigNaocsClient3377.java
@@ -0,0 +1,15 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class CloudAlibabaConfigNaocsClient3377 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudAlibabaConfigNaocsClient3377.class, args);
+    }
+
+}
---
+++ cloud-alibaba-config-naocs-client3377/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author JSQ
+ * @Date 2022/3/1 20:10
+ * @Version 1.0
+ */
+@RestController
+@RefreshScope //支持配置的动态刷新功能
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
